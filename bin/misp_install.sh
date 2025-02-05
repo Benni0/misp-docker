060f1582e9ab4a48380ad46c63682b048256444e
Benni0 <me@benji.at>
2025-02-05 20:15:08 +0100

    misp_install.sh modified

--- bin/misp_install.sh
+++ bin/misp_install.sh
@@ -86,9 +86,9 @@ chown -R apache:apache /var/www/MISP/app/tmp
 #chown -R apache:apache /var/www/MISP/app/files/img/custom
 
 # Create customisations folders and copy default content
-mkdir -p /customize/img_orgs/ /customize/img_custom/
-cp /var/www/MISP/app/files/img/orgs/* /customize/img_orgs/
-chmod 644 /customize/img_orgs/*
+#mkdir -p /customize/img_orgs/ /customize/img_custom/
+#cp /var/www/MISP/app/files/img/orgs/* /customize/img_orgs/
+#chmod 644 /customize/img_orgs/*
 
 # Create alias to cake console command
 echo 'alias cake="su-exec apache /var/www/MISP/app/Console/cake"' >> /root/.bashrc
